2ecc58c4b46b86b438227965deb5be29f1438faf
sandhisarkar <sandhisarkar2@gmail.com>
2023-05-13 16:45:21 +0530

    outward report added

---
+++ ImageHeaven/frmOutwardReport.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace ImageHeaven
+{
+    partial class frmOutwardReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmOutwardReport));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.deButton1 = new nControls.deButton();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.deLabel3 = new nControls.deLabel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.deLabel4 = new nControls.deLabel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.grdStatus = new System.Windows.Forms.DataGridView();
+            this.updateDeedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmsDeeds = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.sfdUAT = new System.Windows.Forms.SaveFileDialog();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdStatus)).BeginInit();
+            this.cmsDeeds.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 65);
+            this.panel1.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.deButton1);
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.deLabel3);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.deLabel4);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(800, 65);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search Criteria";
+            // 
+            // deButton1
+            // 
+            this.deButton1.BackColor = System.Drawing.SystemColors.Control;
+            this.deButton1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("deButton1.BackgroundImage")));
+            this.deButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.deButton1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.deButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.deButton1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deButton1.Location = new System.Drawing.Point(682, 20);
+            this.deButton1.Name = "deButton1";
+            this.deButton1.Size = new System.Drawing.Size(92, 29);
+            this.deButton1.TabIndex = 12;
+            this.deButton1.Text = "&Search";
+            this.deButton1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.deButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.deButton1.UseCompatibleTextRendering = true;
+            this.deButton1.UseVisualStyleBackColor = false;
+            this.deButton1.Click += new System.EventHandler(this.deButton1_Click);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = " ";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(446, 24);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(204, 25);
+            this.dateTimePicker2.TabIndex = 10;
+            // 
+            // deLabel3
+            // 
+            this.deLabel3.AutoSize = true;
+            this.deLabel3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deLabel3.Location = new System.Drawing.Point(378, 27);
+            this.deLabel3.Name = "deLabel3";
+            this.deLabel3.Size = new System.Drawing.Size(52, 15);
+            this.deLabel3.TabIndex = 11;
+            this.deLabel3.Text = "Date To :";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = " ";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(104, 24);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(204, 25);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // deLabel4
+            // 
+            this.deLabel4.AutoSize = true;
+            this.deLabel4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deLabel4.Location = new System.Drawing.Point(25, 29);
+            this.deLabel4.Name = "deLabel4";
+            this.deLabel4.Size = new System.Drawing.Size(68, 15);
+            this.deLabel4.TabIndex = 9;
+            this.deLabel4.Text = "Date From :";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.groupBox2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 450);
+            this.panel3.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.grdStatus);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(800, 450);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Summary :";
+            // 
+            // grdStatus
+            // 
+            this.grdStatus.AllowUserToAddRows = false;
+            this.grdStatus.AllowUserToDeleteRows = false;
+            this.grdStatus.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdStatus.Location = new System.Drawing.Point(3, 71);
+            this.grdStatus.MultiSelect = false;
+            this.grdStatus.Name = "grdStatus";
+            this.grdStatus.ReadOnly = true;
+            this.grdStatus.RowHeadersWidth = 62;
+            this.grdStatus.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdStatus.Size = new System.Drawing.Size(794, 369);
+            this.grdStatus.TabIndex = 7;
+            this.grdStatus.MouseClick += new System.Windows.Forms.MouseEventHandler(this.grdStatus_MouseClick);
+            // 
+            // updateDeedToolStripMenuItem
+            // 
+            this.updateDeedToolStripMenuItem.Name = "updateDeedToolStripMenuItem";
+            this.updateDeedToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.updateDeedToolStripMenuItem.Text = "&Export to Excel";
+            this.updateDeedToolStripMenuItem.Click += new System.EventHandler(this.updateDeedToolStripMenuItem_Click);
+            // 
+            // cmsDeeds
+            // 
+            this.cmsDeeds.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.updateDeedToolStripMenuItem});
+            this.cmsDeeds.Name = "cmsDeeds";
+            this.cmsDeeds.Size = new System.Drawing.Size(181, 48);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 450);
+            this.panel2.TabIndex = 7;
+            // 
+            // frmOutwardReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmOutwardReport";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Outward Report";
+            this.Load += new System.EventHandler(this.frmOutwardReport_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdStatus)).EndInit();
+            this.cmsDeeds.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private nControls.deButton deButton1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private nControls.deLabel deLabel3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private nControls.deLabel deLabel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView grdStatus;
+        private System.Windows.Forms.ToolStripMenuItem updateDeedToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip cmsDeeds;
+        private System.Windows.Forms.SaveFileDialog sfdUAT;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
